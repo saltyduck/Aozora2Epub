41e679758309e3e728d9c8c3a1d8f7634c396bb6
saltyduck <saltyduck@gmail.com>
2024-06-12 23:36:30 +0900

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Aozora2Epub - Blah blah blah
+
+# SYNOPSIS
+
+    use Aozora2Epub;
+
+# DESCRIPTION
+
+Aozora2Epub is
+
+# AUTHOR
+
+Yoshimasa Ueno <saltyduck@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2024- Yoshimasa Ueno
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Aozora2Epub.pm
@@ -0,0 +1,39 @@
+package Aozora2Epub;
+
+use strict;
+use 5.012;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Aozora2Epub - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Aozora2Epub;
+
+=head1 DESCRIPTION
+
+Aozora2Epub is
+
+=head1 AUTHOR
+
+Yoshimasa Ueno E<lt>saltyduck@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2024- Yoshimasa Ueno
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
